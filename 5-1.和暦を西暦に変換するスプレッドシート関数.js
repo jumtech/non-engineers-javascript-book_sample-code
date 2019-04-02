6cf4bc8d0063ae367f733b5346e62507b9cde0ee
jumtech <despairing.clock@gmail.com>
2019-04-03 08:31:40 +0900

    令和対応

--- 5-1.和暦を西暦に変換するスプレッドシート関数.js
+++ 5-1.和暦を西暦に変換するスプレッドシート関数.js
@@ -1,5 +1,11 @@
 function AD(wareki) {
     var ad;
+    if(wareki.indexOf("令和") === 0) {
+      wareki = wareki.replace("令和", "");
+      wareki = wareki.replace("年", "");
+      ad = Number(wareki) + 2018;
+      return ad + "年";
+    }
     if(wareki.indexOf("平成") === 0) {
       wareki = wareki.replace("平成", "");
       wareki = wareki.replace("年", "");
